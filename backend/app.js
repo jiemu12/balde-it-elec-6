4098b2a9ff8839f00ba50b68977a8afbb8ad0d4d
jembalde <07106603@dwc-legazpi.edu>
2025-03-04 14:51:49 +0800

    ads

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post')
+
+mongoose.connect('mongodb+srv://jemmongo:VuvTldACNAbtTUxa@cluster0.kx8s8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+).then(() => {
+    console.log('Connected to database!');
+}).catch(() => {
+    console.log('Connection failed!');
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,7 +24,19 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/api/posts', (req, res, next) => {
+app.post("/api/posts", (req, res, next) => {
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    post.save();
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added succesfully'
+    });
+});
+
+app.get('/api/posts', (req, res, next) => {
     const posts = 
         [
         { 
